cb46d173810cf855591d5a72a7dd133b1569bb61
Nicolias <marat.khakimov.04@gmail.com>
2023-04-26 19:53:05 +0500

    init

---
+++ Assets/Another Assets/PickerWheel/Scripts/WheelPiece.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine ;
+using UnityEngine.UI;
+
+namespace EasyUI.PickerWheelUI 
+{
+    [System.Serializable]
+    public class WheelPiece
+    {
+        [SerializeField] private Prize _prize;
+        [SerializeField] private GameObject _selectFrame;
+        public Prize Prize => _prize;
+        public Sprite Icon => Prize.UIIcon;
+        public string Label => (Prize.PrizeAsInterface as ScriptableObject).name;
+
+        [Tooltip("Reward amount")] public int Amount => Prize.AmountPrize;
+
+        [Tooltip("Probability in %")]
+        [Range(0f, 100f)]
+        public float Chance = 100f;
+
+        [HideInInspector] public int Index;
+        [HideInInspector] public double _weight = 0f;
+
+
+        public void Select()
+        {
+            _selectFrame.SetActive(true);
+        }
+    }
+}
